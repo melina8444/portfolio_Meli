637892369e901233908370339e343bf7d17e6e8b
melina8444 <melinayanguez@gmail.com>
2024-12-15 09:52:15 -0300

    Actualizando...

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToSection(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+}
+
+const toggleTheme = () => {
+  document.body.classList.toggle("dark-mode");
+};
+
+function contactame(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+}
